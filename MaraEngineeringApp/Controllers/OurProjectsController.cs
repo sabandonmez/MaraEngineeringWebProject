30f31d9f15dd9d0ee7ec91eac3638204b88afc5a
Şaban Dönmez <eren.donmez.11@gmail.com>
2024-05-29 17:39:00 +0300

    sayfa tasarımları tamamlandı.

---
+++ MaraEngineeringApp/Controllers/OurProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaraEngineeringApp.Controllers
+{
+    public class OurProjectsController : Controller
+    {
+        public IActionResult OurProjects()
+        {
+            return View();
+        }
+    }
+}
